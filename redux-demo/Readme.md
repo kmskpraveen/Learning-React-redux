392db2e7de8e2f0616a99f884cf3c60a4b32a2be
kmskpraveen <praveen.k@comakeit.com>
2021-04-30 12:49:12 +0530

    6 - Reducers

--- redux-demo/Readme.md
+++ redux-demo/Readme.md
@@ -4,3 +4,8 @@
 3. To specify how the state tree is transformed by actions, you write pure reducers
 
 <img src="/redux-demo/sources/Principles-overview.PNG" alt="drawing" height="380" width="400" />
+
+## Reducers
+- Specify how the app's state changes in response to actions sent to the store
+- Function that accepts state anf action as arguements, and returns the next state of the application
+- (previousState, action) => newState
--- redux-demo/index.ts
+++ redux-demo/index.ts
@@ -1,8 +1,32 @@
-const BUY_CAKE = 'BUY_CAKE'
+const BUY_CAKE = 'BUY_CAKE';
 
+/**
+ * Action creator function
+ * @returns the action object
+ */
 function buyCake() {
     return {
         type: BUY_CAKE,
         info: 'First redux action'    
     }
-} 
+}
+
+const initialState = {
+    numOfCakes: 10
+}
+/**
+ * Reducer function (previousState, action) => newState)
+ * @param state 
+ * @param action 
+ * @returns 
+ */
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case BUY_CAKE: return {
+            ...state,
+            numOfCakes: state.numOfCakes - 1
+        }
+
+        default: return state
+    }
+}
