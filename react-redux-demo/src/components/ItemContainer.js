0c91e3a5826751a39be6a2045b15392d03530326
kmskpraveen <praveen.k@comakeit.com>
2021-05-02 19:10:37 +0530

    27 - mapDispatchToProps

--- react-redux-demo/src/components/ItemContainer.js
+++ react-redux-demo/src/components/ItemContainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { buyCake, buyIceCream } from '../redux';
 
 function ItemContainer(props) {
     return (
         <div>
             <h2>Item - {props.item}</h2>
+            <button onClick={props.buyItem}>Buy Items</button>
         </div>
     )
 }
@@ -21,4 +23,21 @@ const mapStateToProps = (state, ownProps) => {
     }
 }
 
-export default connect(mapStateToProps)(ItemContainer)
+// The second parameter to mapDispatchToProps is the props that
+// have already been passed to the component
+const mapDispatchToProps = (dispatch, ownProps) => {
+    const dispatchFunction = ownProps.cake
+    ? () => dispatch(buyCake())
+    : () => dispatch(buyIceCream());
+
+    return {
+        buyItem: dispatchFunction
+    }
+}
+
+// export default connect(mapStateToProps, mapDispatchToProps)(ItemContainer)
+
+// If you only want to dispatch actions but not subscribe 
+// but not to the state changes to the store, then pass null
+// as first arguement to the connect
+export default connect(null, mapDispatchToProps)(ItemContainer)
