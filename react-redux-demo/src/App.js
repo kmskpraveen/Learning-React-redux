fad990bb747e64b487cfbe1c6c7ebb5918b211b6
kmskpraveen <praveen.k@comakeit.com>
2021-05-02 18:54:09 +0530

    25 - Action payload

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -4,6 +4,7 @@ import HooksCakeContainer from './components/HooksCakeContainer';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import IceCreamContainer from './components/IceCreamContainer';
+import NewCakeContainer from './components/NewCakeContainer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <HooksCakeContainer />
         <CakeContainer />
         <IceCreamContainer />
+        <NewCakeContainer />
       </div>
     </Provider>
   );
